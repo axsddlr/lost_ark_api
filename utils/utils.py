b2bb5677e8820f626ea63bd6e461b2b1d594cad1
Andre Saddler <dresadd09691@gmail.com>
2022-03-16 10:02:27 -0400

    utils: add methods for beautifulsoup get_status & get_soup
    making code more flexible

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,23 @@
+import httpx
+from bs4 import BeautifulSoup
+
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/74.0.3729.169 Safari/537.36",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
 }
+
+
+def get_soup(url):
+    re = httpx.get(url, headers=headers)
+    soup = BeautifulSoup(re.content, 'lxml')
+    if re.status_code == 404:
+        return None
+    else:
+        return soup
+
+
+def get_status(url):
+    re = httpx.get(url, headers=headers)
+    if re.status_code != 200:
+        raise Exception("API response: {}".format(re.status_code))
+    else:
+        return re.status_code

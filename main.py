02850f89e03ead272d2b6628f87088b0af70e5d7
Andre Saddler <dresadd09691@gmail.com>
2022-04-22 23:44:57 -0400

    main: use slowapi for a proper rate limiter

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
+from starlette.requests import Request
 
 from api.scrape import LostA
 from api.scrape_server import LostA_servers
-from ratelimit import limits
+
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(
     title="Unofficial Lost Ark API",
     description="An Unofficial REST API for [Lost Ark](https://www.playlostark.com/en-us/news), Made by [Andre "
                 "Saddler]( "
                 "https://github.com/axsddlr)",
-    version="1.0.2",
+    version="1.0.3",
     docs_url="/",
     redoc_url=None,
 )
@@ -26,16 +31,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# init limiter
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 # init classes
 lost_ark = LostA()
 la_servers = LostA_servers()
 
-TWO_MINUTES = 150
 
-
-@limits(calls=250, period=TWO_MINUTES)
 @app.get("/news/{tag}", tags=["News"])
-def lost_ark_news(tag):
+@limiter.limit("250/minute")
+def lost_ark_news(request: Request, tag):
     """[categories]\n\n
 
     updates \n
@@ -46,18 +53,27 @@ def lost_ark_news(tag):
     return lost_ark.news(tag)
 
 
-@limits(calls=250, period=TWO_MINUTES)
+@app.get("/forums/tips", tags=["Extras"])
+@limiter.limit("250/minute")
+def lost_ark_forums_tips(request: Request):
+    """
+    Guides and Tips via official news section of forums
+    """
+    return lost_ark.la_forums_tips()
+
+
 @app.get("/v1/forums", tags=["News"])
-def lost_ark_forums():
+@limiter.limit("250/minute")
+def lost_ark_forums(request: Request):
     """
     News and Updates via official news section of forums
     """
     return lost_ark.la_forums_v1()
 
 
-@limits(calls=250, period=TWO_MINUTES)
 @app.get("/v2/forums/{category}", tags=["News"])
-def lost_ark_forums(category):
+@limiter.limit("250/minute")
+def lost_ark_forums(request: Request, category):
     """
     [categories]\n
     Maintenance\n
@@ -66,21 +82,21 @@ def lost_ark_forums(category):
     return lost_ark.la_forums_v2(category)
 
 
-@limits(calls=250, period=TWO_MINUTES)
 @app.get("/steam/playercount", tags=["Server Status"])
-def steam_playercount():
+@limiter.limit("250/minute")
+def steam_playercount(request: Request):
     return lost_ark.la_playercount()
 
 
-@limits(calls=250, period=TWO_MINUTES)
 @app.get("/server/all", tags=["Server Status"])
-def lost_ark_all_server_status():
+@limiter.limit("250/minute")
+def lost_ark_all_server_status(request: Request):
     return la_servers.get_server_list_status()
 
 
-@limits(calls=250, period=TWO_MINUTES)
 @app.get("/server/{monitored_servers}", tags=["Server Status"])
-def lost_ark_server_status(monitored_servers):
+@limiter.limit("250/minute")
+def lost_ark_server_status(request: Request, monitored_servers):
     """
     Enter Server Name (Case sensitive)\n
     i.e: http://lostarkapi.herokuapp.com/server/Una\n

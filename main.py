37c96f9dd3346b036849ac6a7e90b78cf2ed2f0f
Andre Saddler <dresadd09691@gmail.com>
2022-05-03 08:54:13 -0400

    main: Use fastapi Request instead of starlette

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
-from starlette.requests import Request
 
 from api.scrape import LostA
 from api.scrape_server import LostA_servers
